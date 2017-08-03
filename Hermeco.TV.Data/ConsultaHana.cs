d335384a11a4ed5b549a3e9a792ffb3acd23e438
jacksson.mosquera@offcorss.com <1@offcorss.com>
2017-08-03 08:25:24 -0500

    Se adicionan dirección y telefono al objeto de inventario tienda

---
+++ Hermeco.TV.Data/ConsultaHana.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+
+namespace Hermeco.TV.Data
+{
+    public class ConsultaHana : HMService
+    {
+        public DataSet ObtenerInfo()
+        {
+            DataSet ds = this.RequestResult();            
+            return ds;           
+        }
+    }
+    
+}
--- Hermeco.TV.Data/Models/InventarioTienda.cs
+++ Hermeco.TV.Data/Models/InventarioTienda.cs
@@ -6,8 +6,7 @@ using System.Web;
 
 namespace Hermeco.TV.Data.Models
 {
-    public class InventarioTienda : HMService
-    {
+    public class InventarioTienda     {
         public string PLU { get; set; }
         public string Material { get; set; }
         public string ValorMat { get; set; }
@@ -15,16 +14,18 @@ namespace Hermeco.TV.Data.Models
         public string Centro { get; set; }
         public string NombreTienda { get; set; }
         public string Direccion { get; set; }
+        public string Telefono { get; set; }
         public string Poblacion { get; set; }
 
         public List<InventarioTienda> ObtenerInventarioTienda(string plu)
         {
-            this.queryParameters.Clear();
-            this.objectName = "ObtenerInventarioTienda";
+            ConsultaHana consulta = new ConsultaHana();
+            consulta.objectName = "ObtenerInventarioTienda";
+            consulta.queryParameters.Clear();           
+            consulta.queryParameters.Add("@plu", plu);
 
             List<InventarioTienda> listInventario = new List<InventarioTienda>();
-            this.queryParameters.Add("@plu", plu);
-            DataSet ds = this.RequestResult();
+            DataSet ds = consulta.ObtenerInfo();
             
 
             if (ds != null)
@@ -39,16 +40,12 @@ namespace Hermeco.TV.Data.Models
                     inventario.Centro = dr["Centro"].ToString();
                     inventario.NombreTienda = dr["Nombre tienda"].ToString();
                     inventario.Poblacion = dr["Poblacion"].ToString();
+                    inventario.Direccion = dr["Direccion"].ToString();
+                    inventario.Telefono = dr["Telefono"].ToString();                      
 
                     listInventario.Add(inventario);
                 }
             }
-
-
-            
-            //this.queryParameters.Add("@fecha1", "20160621");
-            //this.queryParameters.Add("@fecha2", "20160628");
-
             return listInventario;
         }
     }
